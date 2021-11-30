30c6098eeec8694ad64f99629db5c5ee896674c1
Erazem Kokot <mail@erazem.eu>
2021-11-30 21:06:54 +0100

    Clean up machine.go

--- sicsim/sim/machine.go
+++ sicsim/sim/machine.go
@@ -9,25 +9,16 @@ const MAX_ADDRESS = 1048576
 type Machine struct {
 	regs registers
 	mem  [MAX_ADDRESS + 1]byte
-	Devs [256]Device
+	devs [256](*device)
 }
 
 // New creates a new machine
 func (m *Machine) New() {
-	m.Devs[0].New(0) // stdin
-	m.Devs[1].New(1) // stdout
-	m.Devs[2].New(2) // stderr
+	m.devs[0].New(0) // stdin
+	m.devs[1].New(1) // stdout
+	m.devs[2].New(2) // stderr
 
 	if debug {
 		log.Println("Created a new machine")
 	}
-}
-
-// Device returns a device by its number
-func (m *Machine) Device(num byte) Device {
-	if isDevice(num) {
-		return m.Devs[num]
-	}
-
-	return Device{}
 }
