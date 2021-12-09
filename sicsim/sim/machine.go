dd19acb461443610accf00cbbaf9b991e7f6d171
Erazem Kokot <mail@erazem.eu>
2021-12-09 21:20:59 +0100

    Add a ticker

--- sicsim/sim/machine.go
+++ sicsim/sim/machine.go
@@ -2,21 +2,26 @@ package sim
 
 import (
 	"log"
+	"time"
 )
 
 const MAX_ADDRESS = 1048576
 
 type Machine struct {
-	regs registers
-	mem  [MAX_ADDRESS + 1]byte
-	devs [256](*device)
+	regs   registers
+	mem    [MAX_ADDRESS + 1]byte
+	devs   [256](*device)
+	tick   time.Duration
+	ticker *time.Ticker
 }
 
 // New creates a new machine
 func (m *Machine) New() {
-	m.devs[0].New(0) // stdin
-	m.devs[1].New(1) // stdout
-	m.devs[2].New(2) // stderr
+	m.devs[0].New(0)          // stdin
+	m.devs[1].New(1)          // stdout
+	m.devs[2].New(2)          // stderr
+	m.tick = time.Millisecond // Default clock duration
+	m.ticker = nil
 
 	if debug {
 		log.Println("Created a new machine")
---
+++ sicsim/sim/timer.go
@@ -0,0 +1,35 @@
+package sim
+
+import "time"
+
+// Start starts executing commands from memory
+func (m *Machine) Start() {
+	m.ticker = time.NewTicker(m.tick) // Always reset the ticker
+
+	go func() {
+		for range m.ticker.C {
+			m.Execute()
+		}
+	}()
+}
+
+// Stop stops executing commands and stops the machine's ticker
+func (m *Machine) Stop() {
+	m.ticker.Stop()
+	m.ticker = nil
+}
+
+// IsRunning returns the status of the machine's ticker
+func (m *Machine) IsRunning() bool {
+	return m.ticker != nil
+}
+
+// Speed returns the current ticker speed
+func (m *Machine) Speed() time.Duration {
+	return m.tick
+}
+
+func (m *Machine) SetSpeed(kHz int) {
+	// 1 kHz == 1 ms
+	m.tick = time.Duration(kHz * int(time.Millisecond))
+}
