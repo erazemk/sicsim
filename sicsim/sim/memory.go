2a87576128d3eb2a4317fe08742d898814af18eb
Erazem Kokot <mail@erazem.eu>
2021-12-09 20:08:54 +0100

    Update memory internals

--- sicsim/sim/memory.go
+++ sicsim/sim/memory.go
@@ -1,68 +1,50 @@
 package sim
 
-import "encoding/binary"
+import (
+	"encoding/binary"
+	"fmt"
+)
 
 // Byte returns the byte at m[addr]
-func (m Machine) Byte(addr uint32) byte {
+func (m Machine) Byte(addr int) (byte, error) {
 	if isAddr(addr) {
-		return m.mem[addr]
+		return m.mem[addr], nil
 	}
 
-	return 0
+	return 0, fmt.Errorf("not a valid address: %d", addr)
 }
 
 // SetByte sets the byte at the address addr to val
-func (m *Machine) SetByte(addr uint32, val byte) {
+func (m *Machine) SetByte(addr int, val byte) error {
 	if isAddr(addr) {
 		m.mem[addr] = val
+		return nil
 	}
+
+	return fmt.Errorf("not a valid address: %d", addr)
 }
 
 // Word returns the word at m[addr..addr+2]
-func (m Machine) Word(addr uint32) int {
+func (m Machine) Word(addr int) (int, error) {
 	if isAddr(addr) {
-		val := m.mem[addr : addr+3]
-		return int(binary.BigEndian.Uint32(val))
+		word := int(binary.LittleEndian.Uint32(m.mem[addr : addr+2]))
+		return word, nil
 	}
 
-	return -1
+	return 0, fmt.Errorf("not a valid address: %d", addr)
 }
 
 // SetWord sets the word (3 bytes) at addr to val
-func (m *Machine) SetWord(addr uint32, val int) {
+func (m *Machine) SetWord(addr, val int) error {
 	if isAddr(addr) && isWord(val) {
-		bytes := make([]byte, 3)
-		binary.BigEndian.PutUint32(bytes, uint32(val))
+		buf := make([]byte, 4)
+		binary.LittleEndian.PutUint32(buf, uint32(val))
 
-		// TODO: Optimize this shit
-		m.mem[addr] = bytes[0]
-		m.mem[addr+1] = bytes[1]
-		m.mem[addr+2] = bytes[2]
-	}
-}
-
-// Float returns the float at m[addr..addr+5]
-func (m Machine) Float(addr uint32) float64 {
-	if isAddr(addr) {
-		val := m.mem[addr : addr+6]
-		return float64(binary.BigEndian.Uint64(val))
+		m.mem[addr] = buf[0]
+		m.mem[addr+1] = buf[1]
+		m.mem[addr+2] = buf[2]
+		return nil
 	}
 
-	return -1
-}
-
-// SetFloat sets the float (6 bytes) at addr to val
-func (m *Machine) SetFloat(addr uint32, val float64) {
-	if isAddr(addr) && isFloat(val) {
-		bytes := make([]byte, 6)
-		binary.BigEndian.PutUint64(bytes, uint64(val))
-
-		// TODO: Optimize this shit
-		m.mem[addr] = bytes[0]
-		m.mem[addr+1] = bytes[1]
-		m.mem[addr+2] = bytes[2]
-		m.mem[addr+3] = bytes[3]
-		m.mem[addr+4] = bytes[4]
-		m.mem[addr+5] = bytes[5]
-	}
+	return fmt.Errorf("not a valid address or value: %d, %d", addr, val)
 }
