f64587ecec5ec021966b5c76ef1a27c717c0cdea
Erazem Kokot <mail@erazem.eu>
2021-12-14 20:36:36 +0100

    Fix memory implementation and add printing function

--- ass2/sim/memory.go
+++ ass2/sim/memory.go
@@ -27,7 +27,12 @@ func (m *Machine) SetByte(addr int, val byte) error {
 // Word returns the word at m[addr..addr+2]
 func (m Machine) Word(addr int) (int, error) {
 	if isAddr(addr) {
-		word := int(binary.LittleEndian.Uint32(m.mem[addr : addr+2]))
+		buf := make([]byte, 4)
+		buf[0] = 0
+		buf[1] = m.mem[addr]
+		buf[2] = m.mem[addr+1]
+		buf[3] = m.mem[addr+2]
+		word := int(binary.BigEndian.Uint32(buf))
 		return word, nil
 	}
 
@@ -38,13 +43,21 @@ func (m Machine) Word(addr int) (int, error) {
 func (m *Machine) SetWord(addr, val int) error {
 	if isAddr(addr) && isWord(val) {
 		buf := make([]byte, 4)
-		binary.LittleEndian.PutUint32(buf, uint32(val))
+		binary.BigEndian.PutUint32(buf, uint32(val))
 
-		m.mem[addr] = buf[0]
-		m.mem[addr+1] = buf[1]
-		m.mem[addr+2] = buf[2]
+		fmt.Printf("Buf: %[1]v\n", buf)
+
+		// buf[0] == MSB, which is too big for SIC words, so it isn't used
+		m.mem[addr] = buf[1]
+		m.mem[addr+1] = buf[2]
+		m.mem[addr+2] = buf[3]
 		return nil
 	}
 
 	return fmt.Errorf("not a valid address or value: %d, %d", addr, val)
+}
+
+// Mem prints the content of the memory from startAddr to endAddr
+func (m *Machine) Mem(startAddr, endAddr int) string {
+	return fmt.Sprintf("%v", m.mem[startAddr:endAddr])
 }
