4c2e5c8a16ffb781a5dce10c4ed5e700510e7e96
Erazem Kokot <mail@erazem.eu>
2021-12-25 18:59:25 +0100

    Implement a stack for JSUB and RSUB

--- ass2/sim/machine.go
+++ ass2/sim/machine.go
@@ -8,10 +8,14 @@ import (
 
 const MAX_ADDRESS = 1048576
 
+// Stack for JSUB and RSUB instructions
+type stack []int
+
 type Machine struct {
 	regs        registers
 	mem         [MAX_ADDRESS + 1]byte
 	devs        [256](*device)
+	stack       stack
 	tick        time.Duration
 	ticker      *time.Ticker
 	halted      bool
@@ -20,9 +24,10 @@ type Machine struct {
 
 // New creates a new machine
 func (m *Machine) New() {
-	m.NewDevice(0)            // stdin
-	m.NewDevice(1)            // stdout
-	m.NewDevice(2)            // stderr
+	m.NewDevice(0) // stdin
+	m.NewDevice(1) // stdout
+	m.NewDevice(2) // stderr
+	m.stack = stack{}
 	m.tick = time.Millisecond // Default clock duration
 	m.ticker = nil
 
@@ -80,4 +85,14 @@ func (m *Machine) NewDevice(id byte) error {
 
 	m.devs[id] = dev
 	return nil
+}
+
+func (m *Machine) push(val int) {
+	m.stack = append(m.stack, val)
+}
+
+func (m *Machine) pop() int {
+	st := m.stack[len(m.stack)-1]
+	m.stack = m.stack[:len(m.stack)-1]
+	return st
 }
